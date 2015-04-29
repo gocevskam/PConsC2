0eb22e2452a3bda8b679aadc53b6814782e37b6e
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2015-04-29 17:56:31 +0200

    Historical commit with the files as they were before the data layout was changed

--- data_io.py
+++ data_io.py
@@ -5,15 +5,15 @@ import numpy as np
 
 split_re = re.compile(r'[, \t]+')
 
-def read_sequence_names(data_path):
-	with open(data_path + 'sequence_names', 'r') as f:
+def read_sequence_names(file_name):
+	with open(file_name, 'r') as f:
 		return [l.strip() for l in f if l.strip()]
 
-def read_fold_sequence_names(data_path, k):
-	return read_sequence_names(data_path + 'folds/set' + str(k + 1))
+def read_fold_sequence_names(folds_path, k):
+	return read_sequence_names(folds_path + 'set' + str(k))
 
 def read_sequence(data_path, sequence_name):
-	with open(data_path + 'sequences/' + sequence_name + '.fa', 'r') as f:
+	with open(data_path + sequence_name + '/sequence.fa', 'r') as f:
 		for line in f:
 			line = line.strip()
 			if not line.startswith('>'):
@@ -32,15 +32,15 @@ def read_residue_pairs(file_name, separator):
 
 def read_contacts_matrix(data_path, sequence_name, L):
 	contacts = np.zeros((L, L))
-	for (i, j, c) in read_residue_pairs(data_path + 'contacts/' + sequence_name + '.CB', ' '):
+	for (i, j, c) in read_residue_pairs(data_path + sequence_name + '/contacts.CB', ' '):
 		contacts[i-1, j-1] = c
 		contacts[j-1, i-1] = c
 	return contacts
 
-def read_predicted_contacts(data_path, method, sequence_name, L, separation):
+def read_predicted_contacts(data_path, sequence_name, L, method, separation):
 	predicted = []
 	scores = np.zeros((L, L))
-	for (i, j, s) in read_residue_pairs(data_path + method + '/' + sequence_name + '.pred', ',' if 'plmdca' in method else ' '):
+	for (i, j, s) in read_residue_pairs(data_path + sequence_name + '/sequence.fa.' + method, ',' if 'plmdca' in method else ' '):
 		if abs(j - i) >= separation:
 			predicted.append((i, j))
 			scores[i-1, j-1] = s
@@ -49,7 +49,7 @@ def read_predicted_contacts(data_path, method, sequence_name, L, separation):
 	return predicted, scores
 
 def write_predictions(output_path, sequence_name, predicted):
-	with open(output_path + sequence_name + '.pred', 'w') as f:
+	with open(output_path + sequence_name, 'w') as f:
 		predicted.sort(key=lambda (i, j, s): s, reverse=True)
 		for (i, j, s) in predicted:
 			f.write('{} {} {}\n'.format(i, j, s))
